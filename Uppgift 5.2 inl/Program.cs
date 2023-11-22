092c4ac9e6589093ae01d14ab1423f3e73cd57a4
Urkagurka <143172181+Urkagurka@users.noreply.github.com>
2023-11-22 10:55:29 +0100

    Add project files.

---
+++ Uppgift 5.2 inl/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Uppgift_5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] siffror = new int[3];
+            Console.WriteLine("Skriv in ett heltal");
+            siffror[0] = 5;
+            siffror[1] = 3;
+            siffror[2] = int.Parse(Console.ReadLine());
+            Console.Write(siffror[0]);
+            Console.Write(siffror[1]);
+            Console.Write(siffror[2]);
+        }
+
+    }
+
+}
